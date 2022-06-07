797e2b06f65e0eff25e609a88b6f22da8f56e01c
tylertift-bighealth <tyler.tift@bighealth.com>
2022-06-07 10:30:29 -0600

    Replacing play lookup with function

--- typescript/src/statement.ts
+++ typescript/src/statement.ts
@@ -27,7 +27,7 @@ export function statement(
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     const thisAmount = amountFor(perf, play);
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -43,21 +43,25 @@ export function statement(
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
-  function amountFor(perf: PlayPerformance, play: Play): number {
+  function playFor(aPerformance: PlayPerformance): Play {
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance: PlayPerformance, play: Play): number {
     let result = 0;
     switch (play.type) {
       case "tragedy":
         result = 40000;
-        if (perf.audience > 30) {
-          result += 1000 * (perf.audience - 30);
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
         }
         break;
       case "comedy":
         result = 30000;
-        if (perf.audience > 20) {
-          result += 10000 + 500 * (perf.audience - 20);
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
         }
-        result += 300 * perf.audience;
+        result += 300 * aPerformance.audience;
         break;
       default:
         throw new Error(`unknown type: ${play.type}`);
