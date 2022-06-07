fb0b2291336e82bf5ce744252ce033d043ddcf1e
tylertift-bighealth <tyler.tift@bighealth.com>
2022-06-07 10:41:07 -0600

    Split loop for volume credits

--- typescript/src/statement.ts
+++ typescript/src/statement.ts
@@ -22,14 +22,18 @@ export function statement(
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
-    // add volume credits
-    volumeCredits += volumeCreditsFor(perf);
     // print line for this order
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
+
+  for (let perf of invoice.performances) {
+    // add volume credits
+    volumeCredits += volumeCreditsFor(perf);
+  }
+
   result += `Amount owed is ${usd(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
